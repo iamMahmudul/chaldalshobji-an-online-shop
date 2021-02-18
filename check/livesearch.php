0d51e77553f569f1cfb1de440d685d1c208a4361
Mahmudul Hasan <61204842+iamMahmudul@users.noreply.github.com>
2021-02-18 21:23:13 +0600

    Delete check directory

--- check/livesearch.php
+++
@@ -1,7 +0,0 @@
-<?php include ('files/header.php');?>
-<?php
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		$livesearch=$_POST['livesearch'];
-        $livesearch= $search->livesearch($livesearch);
-       }
-?>
